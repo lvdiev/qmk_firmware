e60bd24adf49f71b54882ab1c1401c9b8caf743e
LVDINH-GEAR\lvdinh <dinh@biz4.vn>
2023-04-12 22:28:07 +0700

    add 0x00_lvdinh/bloomer/bluepill

--- keyboards/0x00_lvdinh/bloomer/bluepill/config.h
+++ keyboards/0x00_lvdinh/bloomer/bluepill/config.h
@@ -17,37 +17,32 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEDE //0xFEED
-#define PRODUCT_ID      0x6942 //0x6465
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    lvdinh
-#define PRODUCT         BloomerBluePill
-
 /* key matrix size */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 15
-
-#define MATRIX_COL_PINS { A8, A9, B14, B12, B13, B15, B3, B11, A4, A5, A6, A7, B0, B1, B10 }
-#define MATRIX_ROW_PINS { B5, B6, B7, B9, B8 }
+// GG GG 3v B11 B10 B1 B0 A7 A6 A5 A4 A3 A2 A1 A0 C15 C14 C13
+// microUSB
+// B12 B13 B14 B15 A8 A9 A10 A11 A12 A15 B3 B4 B5 B6 B7 B8 B9
+#define MATRIX_COL_PINS { B11, B10, B1, B0, A7, A6, A5, A4, B3, A9, A8, B15, B14, B13, B12 }
+#define MATRIX_ROW_PINS { B4, B5, B6, B7, B9, B8 }
 
 // #define LED_CAPS_LOCK_PIN A0
 
 #define DIODE_DIRECTION COL2ROW
 
 /* key led setting */
-//#define BACKLIGHT_PIN A8
-//#define BACKLIGHT_PWM_DRIVER PWMD1
-//#define BACKLIGHT_PWM_CHANNEL 1
-//#define BACKLIGHT_LEVELS 6
-//#define BACKLIGHT_BREATHING
-//#define BREATHING_PERIOD 6
+// #define BACKLIGHT_PIN A8
+// #define BACKLIGHT_PWM_DRIVER PWMD1
+// #define BACKLIGHT_PWM_CHANNEL 1
+// #define BACKLIGHT_LEVELS 6
+// #define BACKLIGHT_BREATHING
+// #define BREATHING_PERIOD 6
 
 /* define if matrix has ghost */
-//#define MATRIX_HAS_GHOST
+// #define MATRIX_HAS_GHOST
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCE   5
+#define DEBOUNCE 5
 #define TAPPING_TERM 500
 
 /*
@@ -56,14 +51,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* disable debug print */
-//#define NO_DEBUG
+// #define NO_DEBUG
 
 /* disable print */
-//#define NO_PRINT
+// #define NO_PRINT
 
 /* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_TAPPING
+// #define NO_ACTION_ONESHOT
+// #define NO_ACTION_MACRO
+// #define NO_ACTION_FUNCTION
--- keyboards/0x00_lvdinh/bloomer/bluepill/readme.md
+++ keyboards/0x00_lvdinh/bloomer/bluepill/readme.md
@@ -13,3 +13,7 @@ Make example for this keyboard (after setting up your build environment):
     make handwired/ergocheap:default
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
+* build command for the Bluepill board with 64KB Flash:
+
+    `qmk compile -kb _lvdinh/bloomer/bluepill -km default -e EEPROM_DRIVER=transient`
