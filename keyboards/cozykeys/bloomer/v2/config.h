88b30a37b2a880ac76c97b100754dec8324da8b3
LVDINH-GEAR\lvdinh <dinh@biz4.vn>
2023-04-14 22:24:49 +0700

    migrate configs to info.json

--- keyboards/cozykeys/bloomer/v2/config.h
+++ keyboards/cozykeys/bloomer/v2/config.h
@@ -15,10 +15,3 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
-
-
-// Keyboard Matrix Assignments
-#define MATRIX_ROW_PINS { D0, D1, D3, D2, D4, B2 }
-#define MATRIX_COL_PINS { F7, F6, F5, F4, F1, F0, B1, B4, C6, E6, B5, B6, B7, D6, C7 }
-#define DIODE_DIRECTION COL2ROW
-
