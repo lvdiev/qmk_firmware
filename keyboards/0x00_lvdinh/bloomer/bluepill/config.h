0ed3144b126d4e393b8220f24176ff29c79023cb
LVDINH-GEAR\lvdinh <dinh@biz4.vn>
2023-04-14 21:59:38 +0700

    migrate configs to info.json

--- keyboards/0x00_lvdinh/bloomer/bluepill/config.h
+++ keyboards/0x00_lvdinh/bloomer/bluepill/config.h
@@ -17,48 +17,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 15
-// GG GG 3v B11 B10 B1 B0 A7 A6 A5 A4 A3 A2 A1 A0 C15 C14 C13
-// microUSB
-// B12 B13 B14 B15 A8 A9 A10 A11 A12 A15 B3 B4 B5 B6 B7 B8 B9
-#define MATRIX_COL_PINS { B11, B10, B1, B0, A7, A6, A5, A4, B3, A9, A8, B15, B14, B13, B12 }
-#define MATRIX_ROW_PINS { B4, B5, B6, B7, B9, B8 }
-
-// #define LED_CAPS_LOCK_PIN A0
-
-#define DIODE_DIRECTION COL2ROW
-
-/* key led setting */
-// #define BACKLIGHT_PIN A8
-// #define BACKLIGHT_PWM_DRIVER PWMD1
-// #define BACKLIGHT_PWM_CHANNEL 1
-// #define BACKLIGHT_LEVELS 6
-// #define BACKLIGHT_BREATHING
-// #define BREATHING_PERIOD 6
-
-/* define if matrix has ghost */
-// #define MATRIX_HAS_GHOST
-
-/* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
 #define TAPPING_TERM 500
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-// #define NO_DEBUG
-
-/* disable print */
-// #define NO_PRINT
-
-/* disable action features */
-// #define NO_ACTION_LAYER
-// #define NO_ACTION_TAPPING
-// #define NO_ACTION_ONESHOT
-// #define NO_ACTION_MACRO
-// #define NO_ACTION_FUNCTION
