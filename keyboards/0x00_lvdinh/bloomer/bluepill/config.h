ffd0b79ed9bb85edf9b2b275e2fd10a281b6751c
LVDINH-GEAR\lvdinh <dinh@biz4.vn>
2023-04-15 22:00:50 +0700

    change to use I2C2 port

--- keyboards/0x00_lvdinh/bloomer/bluepill/config.h
+++ keyboards/0x00_lvdinh/bloomer/bluepill/config.h
@@ -16,6 +16,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-
-#define DEBOUNCE 5
-#define TAPPING_TERM 500
--- keyboards/0x00_lvdinh/bloomer/bluepill/keymaps/via/config.h
+++ keyboards/0x00_lvdinh/bloomer/bluepill/keymaps/via/config.h
@@ -5,5 +5,8 @@
 
 #ifdef OLED_ENABLE
     #define USE_I2C
+    #define I2C_DRIVER I2CD2
+    #define I2C1_SCL_PIN B10
+    #define I2C1_SDA_PIN B11
     #define OLED_DISPLAY_128X32
 #endif
